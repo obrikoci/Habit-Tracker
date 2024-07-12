5fe46c0d18cb9cb8b0c4e88738145f3a5ab8eca8
Obri Koci <obrikoci2@gmail.com>
2024-07-12 16:21:40 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import requests
+from datetime import datetime
+
+USERNAME = "obr1"
+TOKEN = "r@nd0mxg3n"
+GRAPHID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)  # -> account got created so this is no longer needed
+
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPHID,
+    "name": "Coding Graph",
+    "unit": "h",
+    "type": "float",
+    "color":"ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)  # -> graph got created so this is no longer needed
+
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}"
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many hours did you code today? "),
+}
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+
+# pixel_update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}/{today.strftime("%Y%m%d")}"
+# updated_pixel_data = {
+#     "quantity": "1.5",
+# }
+# response = requests.put(url=pixel_update_endpoint, json=updated_pixel_data, headers=headers)
+# print(response.text)
+
+
+# pixel_delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}/{today.strftime("%Y%m%d")}"
+# response = requests.delete(url=pixel_update_endpoint, headers=headers)
+# print(response.text)
